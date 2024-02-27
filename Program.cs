c9ebcb0da6065ebccec74a94a95cb5a66e449c78
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-02-27 19:50:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace factorialdowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to calculate its factorial");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int factorial = 1;
+            int i = 1;
+
+            if (number < 0)
+            {
+                Console.WriteLine("Factorial is not defined for negative numbers");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("Factorial of 0 is 1");
+            }
+            else
+            {
+                do
+                {
+                    factorial *= i;
+                    i++;
+                } while (i <= number);
+
+                Console.WriteLine($"Factorial of {number} is {factorial}.");
+            }
+        }
+    }
+}
